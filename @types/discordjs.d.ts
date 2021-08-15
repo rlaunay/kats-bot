238eae07032e605d20a697fe0e800e0e0a3cd243
Remi Launay <rlaunay25@gmail.com>
2021-08-15 19:49:18 +0200

    declaration

--- @types/discordjs.d.ts
+++ @types/discordjs.d.ts
@@ -4,8 +4,6 @@ import { Command } from './../src/interfaces/commands';
 
 declare module 'discord.js' {
   interface Client {
-    prefix: string;
     commands: Collection<string, Command>;
-    cooldowns: Collection<string, Collection<string, number>>;
   }
 }
